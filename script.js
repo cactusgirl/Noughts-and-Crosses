b882dd815692b54a97f4b36093f0c3f973f8aad3
Annabelle Standing <110128555+cactusgirl@users.noreply.github.com>
2022-12-18 16:53:20 +0000

    Tidied code and added comments

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// Turn variable
 var turn = "X"
 
+// Button states
 var values = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+// Winner variable
 var winner = "None"
 
+// Change turn function
 function changeTurn() {
   if (turn == "X") {
     turn = "O";
@@ -12,28 +16,32 @@ function changeTurn() {
   }
 }
 
+// Function to update turns
 function updateTurn() {
   document.getElementById("turnorder").innerHTML = turn;
   setTimeout(updateTurn, 1)
 }
 
+// Function that checks for click
 function checkButton(name, value) {
-      const button = document.getElementById(name);
+    const button = document.getElementById(name);
     button.addEventListener("click", function handleClick() {
-      if (values[value] == 0) {
-        if (turn == "X") {
-          values[value] = 1;
-        } else if (turn == "O") {
-          values[value] = 2;
-        }
-        button.textContent = turn;
-        changeTurn();
-      } 
-    });
-  }
+    if (values[value] == 0) {
+      if (turn == "X") {
+        values[value] = 1;
+      } else if (turn == "O") {
+        values[value] = 2;
+      }
+      button.textContent = turn;
+      changeTurn();
+    } 
+  });
+}
 
+// Check for winner
 function checkWinner() {
   if ((values[0] == values[1] && values[1] == values[2]) && values[0] != 0) {
+    // Dislay winner on screen
     if (values[0] == 1) {
       winner = "Crosses"
     } else if (values[0] == 2) {
@@ -43,3 +51,4 @@ function checkWinner() {
   document.getElementById("winner").innerHTML = winner
   setTimeout(checkWinner, 1)
 }
+
