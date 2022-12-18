0631675efe91ca87a3105782dc2e6d222da5dd4e
Annabelle Standing <110128555+cactusgirl@users.noreply.github.com>
2022-12-18 18:12:16 +0000

    Updated colour scheme

--- script.js
+++ script.js
@@ -18,8 +18,40 @@ function changeTurn() {
 
 // Function to update turns
 function updateTurn() {
-  document.getElementById("turnorder").innerHTML = turn;
+  if (turn == "X") {
+    document.getElementById("turnorder").innerHTML = "Crosses";
+  } else if (turn == "O") {
+    document.getElementById("turnorder").innerHTML = "Noughts";
+  }
   setTimeout(updateTurn, 1)
+}
+
+//Turn box green
+function green(item) {
+  document.getElementById(item).style.backgroundColor = "#51a851";
+  document.getElementById(item).style.borderColor = "#428c42";
+  document.getElementById(item).style.color = "white";
+}
+
+//Turn box amber
+function amber(item) {
+  document.getElementById(item).style.backgroundColor = "#edb658";
+  document.getElementById(item).style.borderColor = "#d4a24e";
+  document.getElementById("grid").style.backgroundColor = "#d4a24e";
+  document.getElementById(item).style.color = "white";
+}
+
+//Turn box blue
+function blue(item) {
+  document.getElementById(item).style.backgroundColor = "#bde2ff";
+  document.getElementById(item).style.borderColor = "#96bad6";      document.getElementById(item).style.color = "#525252";
+}
+
+//Turn box yellow
+function yellow(item) {
+  document.getElementById(item).style.backgroundColor = "#ffffba";
+  document.getElementById(item).style.borderColor = "#c4c487";
+  document.getElementById(item).style.color = "#525252";
 }
 
 // Function that checks for click
@@ -28,20 +60,25 @@ function checkButton(name, value) {
     button.addEventListener("click", function handleClick() {
     if (values[value] == 0 && winner == "None") {
       if (turn == "X") {
+        blue(name)
         values[value] = 1;
       } else if (turn == "O") {
+        yellow(name)
         values[value] = 2;
       }
       button.textContent = turn;
-      button.style.color = "white";
       changeTurn();
     } 
   });
 }
 
+
 // Check for winner
 function checkWinner() {
   if ((values[0] == values[1] && values[1] == values[2]) && values[0] != 0) {
+    green("button1")
+    green("button2")
+    green("button3")
     // Dislay winner on screen
     if (values[0] == 1) {
       winner = "Crosses"
@@ -49,6 +86,9 @@ function checkWinner() {
       winner = "Noughts"
     }
   } else if ((values[3] == values[4] && values[4] == values[5]) && values[3] != 0) {
+    green("button4")
+    green("button5")
+    green("button6")
     // Dislay winner on screen
     if (values[3] == 1) {
       winner = "Crosses"
@@ -56,6 +96,9 @@ function checkWinner() {
       winner = "Noughts"
     }
   } else if ((values[6] == values[7] && values[7] == values[8]) && values[6] != 0) {
+    green("button7")
+    green("button8")
+    green("button9")
     // Dislay winner on screen
     if (values[6] == 1) {
       winner = "Crosses"
@@ -63,6 +106,9 @@ function checkWinner() {
       winner = "Noughts"
     }
   } else if ((values[0] == values[3] && values[3] == values[6]) && values[0] != 0) {
+    green("button1")
+    green("button4")
+    green("button7")
     // Dislay winner on screen
     if (values[0] == 1) {
       winner = "Crosses"
@@ -70,6 +116,9 @@ function checkWinner() {
       winner = "Noughts"
     }
   } else if ((values[1] == values[4] && values[4] == values[7]) && values[1] != 0) {
+    green("button2")
+    green("button5")
+    green("button8")
     // Dislay winner on screen
     if (values[1] == 1) {
       winner = "Crosses"
@@ -77,6 +126,9 @@ function checkWinner() {
       winner = "Noughts"
     }
   } else if ((values[2] == values[5] && values[5] == values[8]) && values[2] != 0) {
+    green("button3")
+    green("button6")
+    green("button9")
     // Dislay winner on screen
     if (values[2] == 1) {
       winner = "Crosses"
@@ -84,6 +136,9 @@ function checkWinner() {
       winner = "Noughts"
     }
   } else if ((values[0] == values[4] && values[4] == values[8]) && values[0] != 0) {
+    green("button1")
+    green("button5")
+    green("button9")
     // Dislay winner on screen
     if (values[0] == 1) {
       winner = "Crosses"
@@ -91,12 +146,26 @@ function checkWinner() {
       winner = "Noughts"
     }
   } else if ((values[2] == values[4] && values[4] == values[6]) && values[6] != 0) {
+    green("button3")
+    green("button5")
+    green("button7")
     // Dislay winner on screen
     if (values[2] == 1) {
       winner = "Crosses"
     } else if (values[2] == 2) {
       winner = "Noughts"
     }
+  } else if (((((((((values[0] != 0) && values [1] != 0) && values[2] != 0) && values[3] != 0) && values[4] != 0) && values[5] != 0) && values[6] != 0) && values[7] != 0) && values[8] != 0) {
+    amber("button1")
+    amber("button2")
+    amber("button3")
+    amber("button4")
+    amber("button5")
+    amber("button6")
+    amber("button7")
+    amber("button8")
+    amber("button9")
+    winner = "Draw"
   }
   document.getElementById("winner").innerHTML = winner
   setTimeout(checkWinner, 1)
