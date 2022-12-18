7ea6041550b292b9edc1a42f8e7f0224751748cd
Annabelle Standing <110128555+cactusgirl@users.noreply.github.com>
2022-12-18 16:31:03 +0000

    Checked one winning instance

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var turn = "X"
 
 var values = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+var winner = "None"
+
 function changeTurn() {
   if (turn == "X") {
     turn = "O";
@@ -19,13 +21,25 @@ function checkButton(name, value) {
       const button = document.getElementById(name);
     button.addEventListener("click", function handleClick() {
       if (values[value] == 0) {
-        button.textContent = turn;
-        changeTurn();
         if (turn == "X") {
           values[value] = 1;
         } else if (turn == "O") {
           values[value] = 2;
         }
+        button.textContent = turn;
+        changeTurn();
       } 
     });
   }
+
+function checkWinner() {
+  if ((values[0] == values[1] && values[1] == values[2]) && values[0] != 0) {
+    if (values[0] == 1) {
+      winner = "Crosses"
+    } else if (values[0] == 2) {
+      winner = "Noughts"
+    }
+  }
+  document.getElementById("winner").innerHTML = winner
+  setTimeout(checkWinner, 1)
+}
