040a66a874e1ff6bf917bb041bf3edb76759b794
Annabelle Standing <110128555+cactusgirl@users.noreply.github.com>
2022-12-18 15:52:18 +0000

    Basic noughts and crosses changing mechanism

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-function checkButton(name) {
+var turn = "X"
+
+var values = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+function changeTurn() {
+  if (turn == "X") {
+    turn = "O";
+  } else if (turn == "O") {
+    turn = "X";
+  }
+}
+
+
+function checkButton(name, value) {
       const button = document.getElementById(name);
-    button.addEventListener("click", function handleClick() {button.textContent = "Clicked"});
-    }
+    button.addEventListener("click", function handleClick() {
+      if (values[value] == 0) {
+        button.textContent = turn;
+        changeTurn();
+        if (turn == "X") {
+          values[value] = 1;
+        } else if (turn == "O") {
+          values[value] = 2;
+        }
+      } 
+    });
+  }
