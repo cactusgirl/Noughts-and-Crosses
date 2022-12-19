6fd93c2bd24d9cdcd7a79fd5c3f0c65082076449
Annabelle Standing <110128555+cactusgirl@users.noreply.github.com>
2022-12-19 17:00:33 +0000

    Winner popup and new font

--- script.js
+++ script.js
@@ -72,102 +72,67 @@ function checkButton(name, value) {
   });
 }
 
+// Display winner
+function displayWinner() {
+  div = document.getElementById("winner display")
+  if (winner == "noughts" || winner == "crosses") {
+    div.innerHTML = "The winner is " + winner
+  } else if (winner == "draw") {
+    div.innerHTML = "It was a draw"
+  }
+  div.style.visibility = "visible"
+}
+
+// Win code
+function win(name1, name2, name3, value) {
+  green(name1)
+  green(name2)
+  green(name3)
+  // Set winner
+  if (values[value] == 1) {
+    winner = "crosses"
+  } else if (values[value] == 2) {
+    winner = "noughts"
+  }
+  displayWinner()
+}
+
+// Draw code
+function draw() {
+  amber("button1")
+  amber("button2")
+  amber("button3")
+  amber("button4")
+  amber("button5")
+  amber("button6")
+  amber("button7")
+  amber("button8")
+  amber("button9")
+  winner = "draw"
+  displayWinner()
+}
 
 // Check for winner
 function checkWinner() {
   if ((values[0] == values[1] && values[1] == values[2]) && values[0] != 0) {
-    green("button1")
-    green("button2")
-    green("button3")
-    // Dislay winner on screen
-    if (values[0] == 1) {
-      winner = "Crosses"
-    } else if (values[0] == 2) {
-      winner = "Noughts"
-    }
+    win("button1", "button2", "button3", 0)
   } else if ((values[3] == values[4] && values[4] == values[5]) && values[3] != 0) {
-    green("button4")
-    green("button5")
-    green("button6")
-    // Dislay winner on screen
-    if (values[3] == 1) {
-      winner = "Crosses"
-    } else if (values[3] == 2) {
-      winner = "Noughts"
-    }
+    win("button4", "button5", "button6", 3)
   } else if ((values[6] == values[7] && values[7] == values[8]) && values[6] != 0) {
-    green("button7")
-    green("button8")
-    green("button9")
-    // Dislay winner on screen
-    if (values[6] == 1) {
-      winner = "Crosses"
-    } else if (values[6] == 2) {
-      winner = "Noughts"
-    }
+    win("button7", "button8", "button9", 6)
   } else if ((values[0] == values[3] && values[3] == values[6]) && values[0] != 0) {
-    green("button1")
-    green("button4")
-    green("button7")
-    // Dislay winner on screen
-    if (values[0] == 1) {
-      winner = "Crosses"
-    } else if (values[0] == 2) {
-      winner = "Noughts"
-    }
+    win("button1", "button4", "button7", 0)
   } else if ((values[1] == values[4] && values[4] == values[7]) && values[1] != 0) {
-    green("button2")
-    green("button5")
-    green("button8")
-    // Dislay winner on screen
-    if (values[1] == 1) {
-      winner = "Crosses"
-    } else if (values[1] == 2) {
-      winner = "Noughts"
-    }
+    win("button2", "button5", "button8", 1)
   } else if ((values[2] == values[5] && values[5] == values[8]) && values[2] != 0) {
-    green("button3")
-    green("button6")
-    green("button9")
-    // Dislay winner on screen
-    if (values[2] == 1) {
-      winner = "Crosses"
-    } else if (values[2] == 2) {
-      winner = "Noughts"
-    }
+    win("button3", "button6", "button9", 2)
   } else if ((values[0] == values[4] && values[4] == values[8]) && values[0] != 0) {
-    green("button1")
-    green("button5")
-    green("button9")
-    // Dislay winner on screen
-    if (values[0] == 1) {
-      winner = "Crosses"
-    } else if (values[0] == 2) {
-      winner = "Noughts"
-    }
+    win("button1", "button5", "button9", 0)
   } else if ((values[2] == values[4] && values[4] == values[6]) && values[6] != 0) {
-    green("button3")
-    green("button5")
-    green("button7")
-    // Dislay winner on screen
-    if (values[2] == 1) {
-      winner = "Crosses"
-    } else if (values[2] == 2) {
-      winner = "Noughts"
-    }
+    win("button3", "button5", "button7", 2)
   } else if (((((((((values[0] != 0) && values [1] != 0) && values[2] != 0) && values[3] != 0) && values[4] != 0) && values[5] != 0) && values[6] != 0) && values[7] != 0) && values[8] != 0) {
-    amber("button1")
-    amber("button2")
-    amber("button3")
-    amber("button4")
-    amber("button5")
-    amber("button6")
-    amber("button7")
-    amber("button8")
-    amber("button9")
-    winner = "Draw"
+    draw()
   }
-  document.getElementById("winner").innerHTML = winner
-  setTimeout(checkWinner, 1)
+  let timeout = setTimeout(checkWinner, 1);
 }
 
